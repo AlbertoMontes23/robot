a8471dece011bb56cc5bb5bad9f79f711de6c810
Alberto <ajaimes@ceti.mx>
2022-09-26 21:43:49 -0500

    senso d

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/jaimes/Documents/2023/Robot/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/jaimes/Documents/2023/Robot/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/jaimes/Documents/2023/Robot/catkin_ws/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/jaimes/Documents/2023/Robot/chapter5_ws/devel;/opt/ros/melodic'.split(';') if '/home/jaimes/Documents/2023/Robot/chapter5_ws/devel;/opt/ros/melodic' != '' else []
--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/jaimes/Documents/2023/Robot/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/jaimes/Documents/2023/Robot/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/jaimes/Documents/2023/Robot/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/jaimes/Documents/2023/Robot/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jaimes/Documents/2023/Robot/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jaimes/Documents/2023/Robot/catkin_ws/src:$ROS_PACKAGE_PATH"
